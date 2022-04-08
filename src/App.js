06841a3b6423b6e2ef0619e15274cf6ae4c542dd
dave <davitgogiberidze101@gmail.com>
2022-04-08 21:27:05 +1000

    Basic Boilerplate Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Boilerplate</h1>
+            <ul>
+                <li>let's separate shit in separate components</li>
+                <li>use material ui idfk</li>
+                <li>lmao</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
