766cfba6515a4554c150903a0f9f3e4b10bde69e
Cornealle <thien.autran75@gmail.com>
2022-04-10 05:04:16 +1000

    Added ability to get roles and houses for politicians

--- server/politicians.py
+++ server/politicians.py
@@ -16,7 +16,9 @@ from bs4 import BeautifulSoup
 import requests
 
 KEY = api_key.OA_KEY
+TVFY_KEY = api_key.tvfy_key
 api_client = OpenAustralia(KEY)
+
 
 mongo_client = pymongo.MongoClient(api_key.MONGO_STRING)
 database = mongo_client["Pondo2022Database"]
@@ -29,18 +31,21 @@ def get_all_names():
         returns: a list of dictionaries
         each dictionary contains:
         "person_id", 
+        "twitter"
         "first",
         "last",
         "party"
     """
     representatives = api_client.get_representatives()
     senators = api_client.get_senators()
-    # print(f"mp is {len(representatives)}, senate is {len(senators)}")
     all_politicians = representatives + senators
     res = []
     for person in all_politicians:
         twitter_handle = get_twitter_handle(f"{person['name']} twitter")
-        politician = {"person_id": person["person_id"], "twitter": twitter_handle, "first": person["first_name"], "last": person["last_name"], "party": person["party"],}
+        house = get_details(person["person_id"])[0]
+        roles = get_details(person["person_id"])[1]
+
+        politician = {"person_id": person["person_id"], "twitter": twitter_handle, "first": person["first_name"], "last": person["last_name"], "party": person["party"], "house": house, "roles": roles}
         res.append(politician)
     return res
 
@@ -62,6 +67,18 @@ def get_twitter_handle(query):
         handle = handle.split("/")[0]
     print(handle)
     return handle
+
+def get_details(id):
+    req = requests.get(f"https://theyvoteforyou.org.au/api/v1/people/{id}.json?key={TVFY_KEY}").json()
+    house = req["latest_member"]["house"]
+    if "representatives" in house.lower():
+        house = "House of Representatives"
+    else: 
+        house = "Senate"
+
+    roles = "".join([role["position"] for role in req["offices"]])
+    return [house, roles]
+
 
 response = get_all_names()
 
