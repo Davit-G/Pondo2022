d5c2d210206d871225e9ba4c035dceae0382c160
Cornealle <thien.autran75@gmail.com>
2022-04-09 17:06:09 +1000

    Added finding twitter handles

--- server/politicians.py
+++ server/politicians.py
@@ -1,5 +1,7 @@
 import api_key
 from openaustralia import OpenAustralia
+from bs4 import BeautifulSoup
+import requests
 
 KEY = api_key.OA_KEY
 api_client = OpenAustralia(KEY)
@@ -20,10 +22,26 @@ def get_all_names():
     all_politicians = representatives + senators
     res = []
     for person in all_politicians:
-        politician = {"person_id": person["person_id"], "first": person["first_name"], "last": person["last_name"], "party": person["party"],}
+        twitter_handle = get_twitter_handle(f"{person['name']} twitter")
+        politician = {"person_id": person["person_id"], "twitter": twitter_handle, "first": person["first_name"], "last": person["last_name"], "party": person["party"],}
         res.append(politician)
     return res
 
+def get_twitter_handle(query):
+    search_response = requests.get(f"https://www.google.com/search?q={query}&start=0").content
+    # print(query)
+    document = BeautifulSoup(search_response, "html.parser").find_all("a")
+    handle = ""
+    for i in range(0, 30):
+        if "/url?q=https://twitter.com/" in document[i]["href"]:
+            handle = document[i]["href"]
+            break
 
-def get_all_parties():
-    pass
+    handle = handle.replace("/url?q=https://twitter.com/", "")
+    handle = handle.split("%3F")[0]
+    if "&" in handle:
+        handle = handle.split("&")[0]
+    if "/" in handle:
+        handle = handle.split("/")[0]
+    # print(handle)
+    return handle
