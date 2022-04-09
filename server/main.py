f6c1726b908d32ea8e7802a0f9a8c36c2af37b72
dave <davitgogiberidze101@gmail.com>
2022-04-09 20:36:20 +1000

    Added CORS policy stuff but prolly doesnt work

--- server/main.py
+++ server/main.py
@@ -1,8 +1,27 @@
 from random import random
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 import api_key
 import pymongo
