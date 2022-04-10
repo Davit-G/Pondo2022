a5908f80c4b51bd23f375b12b6319b08232d5839
dave <davitgogiberidze101@gmail.com>
2022-04-10 19:38:33 +1000

    Finally Finished Front End

--- client/src/App.js
+++ client/src/App.js
@@ -9,19 +9,21 @@ import Header from './components/header';
 import Home from './components/home';
 import Leaderboard from './components/leaderboard';
 import Stats from './components/stats';
+import Landing from './components/landing';
 
 const backend_domain = "http://localhost:8000"
 
-function App() {
+function App({theme}) {
     return (
-        <div>
+        <div className='app' style={{backgroundColor: (theme ? "#0a0d0e" : "#ffffff"), background: (theme ? "linear-gradient(to top, #0a0d0e, #131313, #1d1d1d)" : "linear-gradient(to top, #f1f1f1, #ffffff)")}}>
             <BrowserRouter> {/* Holds state relevant to react-router, ignore this and dont touchey */}
                 <Container>
                     <Stack py={4} spacing={4}>
                         <Header></Header>
 
                         <Routes>
-                            <Route path="/" element={<Home backend_domain={backend_domain}></Home>} />
+                            <Route path="/" element={<Landing backend_domain={backend_domain}></Landing>} />
+                            <Route path="/game" element={<Home backend_domain={backend_domain}></Home>} />
                             <Route path="leaderboard" element={<Leaderboard backend_domain={backend_domain} query={"/politicians"}></Leaderboard>} />
                             <Route path="stats" element={ <Stats backend_domain={backend_domain}></Stats>} />
                         </Routes>
