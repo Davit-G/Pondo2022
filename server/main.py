87b385895ec4ade2e1c73a3fea171d3d3bc4ade6
dave <davitgogiberidze101@gmail.com>
2022-04-10 16:07:13 +1000

    Add Parties For Policy integration on backend

--- server/main.py
+++ server/main.py
@@ -102,16 +102,58 @@ async def get_all_policies():
 
 
 
+@app.get('/parties_to_policy_broke/')  # SPAGHET CODE DONT TOUCH
+async def parties_for_policyBROKE(id: str):
+    print(id)
+    participants = list(policies.find( {"id": int(id)}, {"_id": 0, "participants": 1}))
+    partiesList = [{}]
+    #return participants
+    for participant in participants[0]["participants"]: # people in the policy request, it;s multiple people
+        for i, party in enumerate(partiesList):
+            found_pol = politicians.find_one({"person_id": participant["person_id"]},{"_id": 0, "party": 1})
+            if "party" in party:
+                if party["party"] is found_pol["party"]: # if we found the party that participant belongs to
+                    if participant["voted"]:
+                        partiesList[i]["numVotes"] += 1
+                        partiesList[i]["agreementSum"] += participant["agreement"]
+                        break
+                    else:
+                        break #dont do anything if voter didnt vote
+                else: 
+                    continue # go to other ppl bru
+            
+        else:
+            partiesList.append({
+                "numVotes": 0,
+                "agreementSum": 0,
+                "party": found_pol["party"]
+            })
+    return partiesList
+
 @app.get('/parties_to_policy/')
 async def parties_for_policy(id: str):
-    participants = policies.find({"id": id}, {"_id": 0, "participants": 1})
-    partiesList = parties.find({}, {"_id": 0, "name": 1})
-    for policy_participants in participants:
-        for participant in policy_participants:
-            pass
-    print(partiesList)
-    pass
+    print(id)
+    participants = list(policies.find( {"id": int(id)}, {"_id": 0, "participants": 1}))
+
+
+
+    partiesList = {}
+    for party in parties.find({}, {"name": 1, "_id": 0}):
+        partiesList[party["name"]] = {
+            "numVotes":0,
+            "agreementSum":0,
+            "party": party["name"]
+        }
+
+    #return participants
+    for participant in participants[0]["participants"]: # people in the policy request, it;s multiple people
         
+        found_pol = politicians.find_one({"person_id": participant["person_id"]},{"_id": 0, "party": 1})
+        if participant["voted"]:
+            partiesList[found_pol["party"]]["numVotes"] += 1
+            partiesList[found_pol["party"]]["agreementSum"] += float(participant["agreement"])
+    return partiesList
+
     
 
 @app.post('/vote/')
